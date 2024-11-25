f0131efadb491e68aa6c47b4f10c3f700f7aee09
pml0607 <phammanhlinh680@gmail.com>
2024-11-25 09:16:33 +0700

    add topic

--- src/mqtt/topics.py
+++ src/mqtt/topics.py
@@ -1,5 +1,5 @@
-TOPICS = [
-    "topic/results/#",
-    "topic/face_terminal/sync_request/#",
-    "topic/Device/AuthenToken/#"
-]
+TOPICS = {
+    'RESULTS': 'topic/results/#',
+    'SYNC_REQUEST': 'topic/face_terminal/sync_request/',
+    'AUTH_TOKEN': 'topic/Device/AuthenToken/'
+}
