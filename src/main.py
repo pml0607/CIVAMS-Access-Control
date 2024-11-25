10b06390f8b12d1c9d506ce6f8c9854f53b144e2
pml0607 <phammanhlinh680@gmail.com>
2024-11-25 10:21:41 +0700

    main program (setting up)

---
+++ src/main.py
@@ -0,0 +1,45 @@
+from mqtt.client import MqttClient
+from logic.access import AccessControl
+from logic.db_sync import DatabaseSync
+from logic.auth import AuthManager
+from device.door_control import DoorControl
+from database.db import Database
+
+# Khởi tạo các module
+mqtt_client = MqttClient()
+access_control = AccessControl()
+db_sync = DatabaseSync()
+auth_manager = AuthManager()
+door_control = DoorControl()
+db = Database()
+
+def main():
+    mqtt_client.connect()
+    
+    # Đăng ký các topic
+    mqtt_client.subscribe("topic/results/#", handle_results)
+    mqtt_client.subscribe("topic/face_terminal/sync_request/", handle_sync_request)
+    mqtt_client.subscribe("topic/Device/AuthenToken/", handle_auth_token)
+
+    # Khởi động chương trình
+    mqtt_client.loop_forever()
+
+def handle_results(msg):
+    """Xử lý kết quả nhận từ MQTT message (face recognition)"""
+    # Kiểm tra quyền truy cập
+    if access_control.check_access(msg):
+        door_control.open_door()
+    else:
+        door_control.close_door()
+
+def handle_sync_request(msg):
+    """Xử lý yêu cầu đồng bộ dữ liệu"""
+    db_sync.sync(msg)
+
+def handle_auth_token(msg):
+    """Lưu token xác thực và đồng bộ với server"""
+    auth_manager.update_auth_token(msg)
+    db_sync.sync(msg)
+
+if __name__ == "__main__":
+    main()
