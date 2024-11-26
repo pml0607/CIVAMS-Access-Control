5e77591498dbc188e09d151f832ac05370ea78dc
pml0607 <phammanhlinh680@gmail.com>
2024-11-26 04:31:31 +0000

    env sync

--- src/device/door_control.py
+++ src/device/door_control.py
@@ -1,18 +1,16 @@
 import time
-from gpio_setup import setup_gpio
+from gpio_setup import GPIO_Setup
+from pyA20.gpio import port, gpio
 
 class DoorControl:
-    def __init__(self):
-        self.door_pin = setup_gpio()
-
-    def open_door(self, duration=5):
-        """Mở cửa trong một khoảng thời gian (giây)"""
-        print("Opening door...")
-        # Điều khiển GPIO để mở cửa
-        time.sleep(duration)
-        self.close_door()
-
-    def close_door(self):
-        """Đóng cửa"""
-        print("Closing door...")
-        # Điều khiển GPIO để đóng cửa
+	def __init__(self, pin = port.PA7):
+		self.gpio_controller = GPIO_Setup(pin = pin, mode = gpio.OUTPUT)
+		self.gpio_controller.setup_gpio()
+	def open_door(self, duration = 5):
+		print("Opening door...")
+		self.gpio_controller.set_state(True)
+		time.sleep(duration)
+		self.close_door()
+	def close_door(self):
+		print("Closing door...")
+		self.gpio_controller.set_state(False)
--- src/device/gpio_setup.py
+++ src/device/gpio_setup.py
@@ -1,8 +1,36 @@
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 
-def setup_gpio():
-    """Cấu hình GPIO cho việc điều khiển relay (mở/đóng cửa)"""
-    gpio.init()
-    gpio.setcfg(port.PA7, gpio.OUTPUT)
-    return 7
+
+class GPIO_Setup:
+    def __init__(self, pin, mode):
+	self.pin = pin
+	self.mode = mode
+	self.state = None
+	self.initialized = False
+
+
+    def setup_gpio(self):
+        """Cấu hình GPIO cho việc điều khiển relay (mở/đóng cửa)"""
+        gpio.init()
+        gpio.setcfg(self.pin, self.mode)
+        self.initialized = True
+	if self.mode == gpio.OUTPUT:
+	    self.state = gpio.LOW
+	    gpio.output(self.pin, self.state)
+
+    def set_state(self.state):
+	if not self.initialized:
+	    raise RuntimeError("GPIO chưa được khởi tạo!")
+	if self.mode != gpio.OUTPUT:
+	    raise RuntimeError("GPIO không ở ché độ OUTPUT!")
+	self.state = gpio.HIGH if state else gpio.LOW
+	gpio.output(self.pin, self.state)
+
+    def get_state(self):
+        """Lấy trạng thái hiện tại của GPIO"""
+        if not self.initialized:
+            raise RuntimeError("GPIO chưa được khởi tạo!")
+        if self.mode == gpio.INPUT:
+            self.state = gpio.input(self.pin)
+        return self.state
