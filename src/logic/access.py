9fa9a2de456bbc173ff3f0acf328f7f675fa11bd
pml0607 <phammanhlinh680@gmail.com>
2024-11-27 14:36:56 +0700

    .

--- src/logic/access.py
+++ src/logic/access.py
@@ -23,17 +23,17 @@ class AccessControl:
 
         #Xử lý thời gian
 
-        if not isinstance(update_at, str):
+        if not isinstance(updateAt, str):
                 print(f"Lỗi: `updateAt` không phải chuỗi, giá trị nhận được: {update_at}")
                 return False
 
         try:
             # Chuyển đổi thời gian từ định dạng ISO 8601 với hậu tố Z
-            if update_at.endswith("Z"):
-                update_at = update_at.replace("Z", "+00:00")
-            current_time = datetime.fromisoformat(update_at)
+            if updateAt.endswith("Z"):
+                updateAt = updateAt.replace("Z", "+00:00")
+            current_time = datetime.fromisoformat(updateAt)
         except ValueError as e:
-            print(f"Thời gian không hợp lệ trong `updateAt`: {update_at}. Lỗi: {e}")
+            print(f"Thời gian không hợp lệ trong `updateAt`: {updateAt}. Lỗi: {e}")
             return False
         current_day = current_time.strftime("%a")
         current_hour = current_time.strftime("%H:%M")
