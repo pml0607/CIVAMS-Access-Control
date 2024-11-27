8903018533f271a2252ceef9825020e3ab37c8aa
pml0607 <phammanhlinh680@gmail.com>
2024-11-27 11:08:47 +0700

    database mocule setup

--- src/database/db.py
+++ src/database/db.py
@@ -1,17 +1,40 @@
 import sqlite3
 
-conn = sqlite3.connect('access_control.db')
-cursor = conn.cursor()
+class Database:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+        self.cursor = None
 
+    def connect(self):
+        if self.conn is None:
+            self.conn = sqlite3.connect(self.db_name)
+            self.cursor = self.conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS access_rules (
-               id INTERGER PRIMARY KEY, 
-               recognize_id TEXT, 
-               department_id TEXT, 
-               access_allowed INTEGER, 
-               access_days TEXT, 
-               start_time TEXT, 
-               end_time TEXT )
-               ''')
+    def execute(self, query, params = ()):
+        self.connect()
+        self.cursor.execute(query, params)
+        self.conn.commit()
+    
+    def fetchall(self):
+        """Lấy tất cả kết quả từ truy vấn."""
+        return self.cursor.fetchall()
 
-conn.commit()
+    def fetchone(self):
+        """Lấy một kết quả từ truy vấn."""
+        return self.cursor.fetchone()
+
+    def close(self):
+        """Đóng kết nối cơ sở dữ liệu."""
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+
+    def access(self, user, department):
+        self.excute('''SELECT access_allowed, access_days, start_time, end_time
+                    FROM access_control
+                    WHERE recognize_id = ? AND department_id = ?
+                    ''', (user, department))
+        rule = self.fetchone()
+        return rule
